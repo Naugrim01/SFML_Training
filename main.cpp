e7f62e412296b4a1235c266f283837ec2c49401d
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2023-07-30 17:39:17 +0200

    include SFML lib into the project

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include <SFML\Graphics.hpp>
+#include <SFML\System.hpp>
+#include <SFML\Window.hpp>
+#include <SFML\Audio.hpp>
+#include <SFML\Network.hpp>
+
+
+int main()
+{
+	std::cout << "SFML connected\n";
+
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+
+	system("pause");
+	return 0;
+}
